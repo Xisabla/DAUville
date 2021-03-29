0b9a42e40d3c2aba2a724d983a09eef1e77e24c9
Gautier Miquet <xisabla.dev@gmail.com>
2021-03-29 16:44:36 +0200

    add react support on build

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+	render() {
+		return (
+			<div className="app">
+				<h1>Hello World</h1>
+			</div>
+		)
+	}
+}

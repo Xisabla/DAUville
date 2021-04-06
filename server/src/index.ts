fee54512a8d93239ab03f8b8c4383d96325cc96c
Gautier Miquet <xisabla.dev@gmail.com>
2021-04-06 17:48:37 +0200

    begin user module to handle user connection, registration and disconnection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,64 +1,12 @@
-import { Request, Response } from 'express'
-import { Socket } from 'socket.io'
-
 import config from './config'
-import { Application, EndpointPath, Module } from './core/'
-
-// Note: This is just an example for testing
-class MyModule extends Module {
-	constructor(app: Application) {
-		super(app, 'MyModule')
-
-		this.addEndpoints([
-			// HTTP endpoint
-			{ type: 'HTTP', path: '/test', method: 'GET', handle: this.testHandler.bind(this) },
-			// Socket endpoint
-			{ type: 'Socket', path: '/getUsername', handle: this.getUsernameHandler.bind(this) }
-		])
-
-		this.registerTask('*/10 * * * * *', this.taskActionMethod.bind(this))
-	}
-
-	/**
-	 * /getUsername socket handler
-	 */
-	public getUsernameHandler(path: EndpointPath, data: any[], socket: Socket): any {
-		return socket.emit('response', {
-			path,
-			username: 'test'
-		})
-	}
-
-	/**
-	 * /test HTTP handler
-	 */
-	public testHandler(req: Request, res: Response) {
-		this._log(`Handling request ${req.url} from ${req.ip}`)
-		return res.json({ message: 'Hello world, this is just a test HTTP endpoint' })
-	}
-
-	/**
-	 * Task action
-	 */
-	public async taskActionMethod(): Promise<any> {
-		console.log(`Task triggered (every 10s)`)
-
-		return Promise.resolve()
-	}
-
-	/**
-	 * Log when a socket joins and gets spread
-	 */
-	public onSocketJoin(socket: Socket): void {
-		this._log(`Socket ${socket.id} connected (caught from ${this.name})`)
-	}
-}
+import { Application } from './core/'
+import { UserModule } from './modules'
 
 // ---- App ------------------------------------------------------------------------------
 const app = new Application({ ...config.app, ...{ db: config.db } })
 
 // Register modules
-app.registerModule(MyModule)
+app.registerModule(UserModule)
 
 // Run the server
 app.run()
---
+++ server/src/modules/UserModule.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express'
+
+import { Application, Module } from '../core'
+import { IUserSchema, User } from '../models/User'
+
+// NOTE: Small tweak to define possible session content from express
+// See https://stackoverflow.com/a/65696437
+declare module 'express-session' {
+	interface SessionData {
+		user?: IUserSchema
+	}
+}
+
+// ---- Module ---------------------------------------------------------------------------
+export class UserModule extends Module {
+	constructor(app: Application) {
+		super(app, 'UserModule')
+
+		this.addEndpoint({ type: 'HTTP', method: 'POST', path: '/login', handle: this.postLoginHandler.bind(this) })
+	}
+
+	/**
+	 * Handle /login POST route: Attempt to authenticate the user and send its information back
+	 */
+	public async postLoginHandler(req: Request, res: Response): Promise<void> {
+		const { email, password } = req?.query ?? {}
+
+		try {
+			// Check if email and password are not undefined
+			if (email && password) {
+				const user = await User.authenticate(email as string, password as string)
+
+				// Check if there is user
+				if (user) {
+					// Hide user password before sending it to the client
+					user.password = '********'
+
+					req.session.user = user
+					res.json({ user: user.toJSON() })
+				} else {
+					// No user found --> Error: Invalid credentials
+					res.json({
+						error: 'Invalid credentials',
+						message: 'No user matching the given credentials'
+					})
+				}
+			} else {
+				// No email or password --> Error: Missing arguments
+				res.json({
+					error: 'Missing arguments',
+					message: 'Missing one or many of the following parameters: email,password'
+				})
+			}
+		} catch (error) {
+			// Error caught --> Something went wrong
+			res.json({
+				error: 'Unexpected error',
+				message: 'Something went wrong',
+				details: error
+			})
+		}
+
+		return res.end()
+	}
+
+	// TODO: register: check rights of the creator (send errors), create and save user (sens errors), end: send info
+	// TODO: disconnect: get user by token (skip errors), break token: send info/error
+}

2560b7ee70b4554cdfc8faa7a731958e0cd0b974
Gautier Miquet <xisabla.dev@gmail.com>
2021-04-06 18:26:43 +0200

    generate random secret if no one is set

--- server/src/config/security.ts
+++ server/src/config/security.ts
@@ -1,6 +1,8 @@
+import { v4 as uuid } from 'uuid'
+
 import { defaultEnv } from '.'
 
 export default {
 	saltRounds: parseInt(defaultEnv('SALT_ROUNDS', '10')),
-	secret: defaultEnv('SECRET', 'secretpassphrasehere')
+	secret: defaultEnv('SECRET', uuid())
 }

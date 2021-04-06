bdded9cacbd99b5382fdeff5c14b80e95ebcd946
Gautier Miquet <xisabla.dev@gmail.com>
2021-04-06 17:47:40 +0200

    add methods to authenticate the user

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,9 @@
 import { compare, hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 import { Document, Model, model, Schema } from 'mongoose'
+import { v4 as uuid } from 'uuid'
+
+import config from '../config'
 
 // ---- Schema interface -----------------------------------------------------------------
 export interface IUserSchema extends Document {
@@ -7,17 +11,31 @@ export interface IUserSchema extends Document {
 	email: string
 	/** Hashed password of the user */
 	password: string
+	/** Account type: user (normal user) or admin */
+	type: 'ADMIN' | 'USER'
+	/** JSON Web Token for authentication */
+	token?: string
 
 	// Methods
 	/** Check if the given plain text password matchs the user's password */
 	checkPassword(password: string): Promise<boolean>
+	/** Generate a token for the user */
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	generateToken(params?: object): Promise<string>
 }
 
 // ---- Schema ---------------------------------------------------------------------------
 export const UserSchema = new Schema<IUserSchema, Model<IUserSchema>>(
 	{
 		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true }
+		password: { type: String, required: true },
+		type: {
+			type: String,
+			required: true,
+			default: 'ADMIN',
+			validate: (val: string) => ['ADMIN', 'USER'].includes(val.toUpperCase())
+		},
+		token: { type: String, required: false, default: null }
 	},
 	{ collection: 'users' }
 )
@@ -31,6 +49,9 @@ export interface IUser extends Model<IUserSchema> {
 	hashPassword(password: string): Promise<string>
 	/** Check if a hashed password correspond to a plain text password */
 	checkPassword(password: string, hash: string): Promise<boolean>
+	/** Check for user credentials, generate its token and return information */
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	authenticate(email: string, password: string, params?: object): Promise<IUserSchema | false>
 }
 
 // ---- Methods --------------------------------------------------------------------------
@@ -41,6 +62,25 @@ export interface IUser extends Model<IUserSchema> {
  */
 UserSchema.methods.checkPassword = async function (password: string): Promise<boolean> {
 	return await User.checkPassword(password, this.password)
+}
+
+/**
+ * Generate a token for the user
+ * @param params Parameters to add to the token
+ * @returns The generated JWT
+ */
+UserSchema.methods.generateToken = async function (params = {}): Promise<string> {
+	// Add a UUID to the parameters to make sure it will be a new token
+	params._uuid = uuid()
+
+	// Create the token and
+	const token = sign({ ...{ user: this }, ...{ params } }, config.security.secret)
+
+	// Assign and save
+	this.token = token
+	await this.save()
+
+	return token
 }
 
 // ---- Statics --------------------------------------------------------------------------
@@ -61,8 +101,7 @@ UserSchema.statics.doesExist = async function (email: string): Promise<boolean> 
  * @returns The hashed password
  */
 UserSchema.statics.hashPassword = async function (password: string): Promise<string> {
-	// TODO: Put the salt round count in the config instead of hard coding it, so it is easy to change
-	return await hash(password, 10)
+	return await hash(password, config.security.saltRounds)
 }
 
 /**
@@ -73,6 +112,31 @@ UserSchema.statics.hashPassword = async function (password: string): Promise<str
  */
 UserSchema.statics.checkPassword = async function (password: string, hash: string): Promise<boolean> {
 	return await compare(password, hash)
+}
+
+/**
+ * Check for user credentials, generate its token and return information
+ * @param email Email credentials
+ * @param password Password credentials
+ * @param params Parameters to given to the token creation (default: undefined)
+ * @returns The user instance if a user matches the credentials, false otherwise
+ */
+UserSchema.statics.authenticate = async function (
+	email: string,
+	password: string,
+	params = {}
+): Promise<IUserSchema | false> {
+	const user = await User.findOne({ email })
+
+	// If a user is found and the provided password matches
+	if (user && (await user.checkPassword(password))) {
+		// Generates new user token
+		await user.generateToken(params)
+
+		return user
+	}
+
+	return false
 }
 
 // ---- Model ----------------------------------------------------------------------------

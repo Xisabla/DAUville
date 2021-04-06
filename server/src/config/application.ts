91467d341a2c564e0e7c12eba4738acb71ffdb73
Gautier Miquet <xisabla.dev@gmail.com>
2021-04-06 17:30:52 +0200

    minor fix on env variable 'PUBLIC' which is usualy set by the OS, so renamed it to PUBLIC_PATH

--- server/src/config/application.ts
+++ server/src/config/application.ts
@@ -3,6 +3,6 @@ import path from 'path'
 import { defaultEnv, devProdEnv } from '.'
 
 export default {
-	public: path.resolve(defaultEnv('PUBLIC', '../client/public')),
+	public: path.resolve(defaultEnv('PUBLIC_PATH', '../client/public')),
 	port: parseInt(devProdEnv('PORT_DEV', 'PORT', '3000'), 10)
 }

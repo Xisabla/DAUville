5acf5cf54e56aa2f9c8a615206ff461ef460c200
Gautier Miquet <xisabla.dev@gmail.com>
2021-04-09 16:58:18 +0200

    add comments and minor typo fix

--- server/src/models/Measure.ts
+++ server/src/models/Measure.ts
@@ -1,27 +1,39 @@
-import mongoose, { Schema, Document, Model, model } from 'mongoose';
+import { Document, Model, model, Schema } from 'mongoose'
 
+/**
+ * Sensor that recorded the measure
+ */
 export const enum Sensor {
-    Ph,
-    Humidity,
-    AirTemperature,
-    WaterTemperature,
-    ExternalAirHumidity,
-    ExternalAirTemperature
+	Ph,
+	Humidity,
+	AirTemperature,
+	WaterTemperature,
+	ExternalAirHumidity,
+	ExternalAirTemperature
 }
 
+// ---- Schema interface -----------------------------------------------------------------
 export interface IMeasureSchema extends Document {
-    sensor: number,
-    captureDate: Date,
-    value: number
+	/** Sensor the recorded the measure */
+	sensor: number
+	/** Date and time of the measure record */
+	captureDate: Date
+	/** Value of the measure (unit not given) */
+	value: number
 }
 
+// ---- Schema ---------------------------------------------------------------------------
 export const MeasureSchema = new Schema<IMeasureSchema, Model<IMeasureSchema>>(
-    {
-        sensor: {type: Number, required: true},
-        captureDate: {type: Date, required: true},
-        value: {type: Number, required: true}
-    }, 
-    { collection: 'measures' }
+	{
+		sensor: { type: Number, required: true },
+		captureDate: { type: Date, required: true },
+		value: { type: Number, required: true }
+	},
+	{ collection: 'measures' }
 )
 
-export const Measure = model<IMeasureSchema>('Measure', MeasureSchema)
+// ---- Model Interface ------------------------------------------------------------------
+export type IMeasure = Model<IMeasureSchema>
+
+// ---- Model ----------------------------------------------------------------------------
+export const Measure = model<IMeasureSchema, IMeasure>('Measure', MeasureSchema)

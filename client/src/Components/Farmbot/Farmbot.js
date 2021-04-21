989ebef1ec39f9ffe55cbfd5e238f92834a6b9c2
Gautier Miquet <xisabla.dev@gmail.com>
2021-04-21 22:36:24 +0200

    minor change on the json of the farmbot

--- client/src/Components/Farmbot/Farmbot.js
+++ client/src/Components/Farmbot/Farmbot.js
@@ -1,46 +1,64 @@
 import React, { Component } from 'react'
-import { JsonToTable } from "react-json-to-table";
+import { JsonToTable } from 'react-json-to-table'
 /**
  * Component that shows the main values of the farmbot information
  */
 
 export class Farmbot extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
+
 		this.state = {
-		  items: [],
-		  isLoaded: false,
-		};
-	  }
+			items: [],
+			isLoaded: false
+		}
+	}
+
 	componentDidMount() {
-		const sumups = fetch('/getFarmbotDailySumUp')
+		fetch('/getFarmbotDailySumUp')
 			.then((res) => res.json())
 			.then((data) => {
-				this.setState({
-					isLoaded: true,
-					items: data
-				  });
 				if (data.error) {
-					console.log('')
+					console.log('An error has occurred while fetching data')
 				} else {
-					const todaySumup = data[data.length - 1]
-					//console.log(data) // { date: ..., completedSequences: [ ... ], uncompletedSequences: [ ... ], errorLogs: [ ... ] }
+					this.setState({
+						isLoaded: true,
+						items: data.map((elem) => {
+							return {
+								date: elem.date,
+								completedSequences: elem.completedSequences,
+								uncompletedSequences: elem.uncompletedSequences,
+								errorLogs: elem.errorLogs.map((log) => {
+									return {
+										updated_at: log.updated_at,
+										message: log.message,
+										type: log.type,
+										x: log.x,
+										y: log.y,
+										z: log.z
+									}
+								})
+							}
+						})
+					})
 				}
 			})
 	}
 	render() {
-		const { items } = this.state;
+		const { items } = this.state
 
 		if (!this.state.isLoaded) {
-		  return <div>Loading ... </div>;
+			return <div>Loading ... </div>
 		} else {
-		  return (
-			<div className="content">
-			<div className="container-fluid">
-			{items.map(item => <JsonToTable json={item} />)}
-			</div >
-				</div >
-		  );
+			return (
+				<div className="content">
+					<div className="container-fluid">
+						{items.map((item, index) => (
+							<JsonToTable key={index} json={item} />
+						))}
+					</div>
+				</div>
+			)
 		}
-	  }
+	}
 }
